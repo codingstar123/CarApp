e82bbe75fb1d24436b93eb287baf255f5776fc40
Ashley Capace <acapace@yahoo.com>
2022-12-08 14:19:52 -0500

    set up schema

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+
+
+////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////
 
 //========Route=======
 app.get('/', (req,res)=>{
@@ -11,3 +20,12 @@ app.get('/', (req,res)=>{
 app.listen(3000, ()=>{
   console.log('listening...');
 })
+ 
+
+////////////////////////////////////////////////////////////////
+
+mongoose.connect('mongodb://localhost:27017/carapp')
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongod...');
+});
+
