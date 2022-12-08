0024afa9a23f5c98ae560f217329b24864e773a4
Ashley Capace <acapace@yahoo.com>
2022-12-08 16:12:12 -0500

    completed all routes

--- server.js
+++ server.js
@@ -1,19 +1,57 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
-
+const Cars = require('./models/cars.js');
 
 ////////////////////////////////////////////////////////////////
 
-
+app.use(express.json());
+app.use(cors());
 
 ////////////////////////////////////////////////////////////////
 
 //========Route=======
+
 app.get('/', (req,res)=>{
   res.send('Hello World')
 })
+
+
+/////CREATE/POST ROUTE/////Add Car 
+app.post('/cars', (req, res) => {
+  Cars.create(req.body, (err, createdCar) =>{
+      res.json(createdCar);
+  });
+});
+
+///////INDEX ROUTE/GET ROUTE/READ////Get Car
+
+app.get('/cars', (req, res) => {
+  Cars.find({}, (err, foundCar) => {
+      res.json(foundCar);
+  });
+});
+
+
+///////DELETE ROUTE///Delete Cars
+
+app.delete('/cars/:id', (req, res)=>{
+  Cars.findByIdAndRemove(req.params.id, (err, deletedCars)=>{
+      res.json(deletedCars);
+  });
+});
+
+
+///////UPDATE/EDIT ROUTE///Edit Cars
+
+app.put('/cars/:id', (req, res)=>{
+  Cars.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedCar)=>{
+      res.json(updatedCar);
+  });
+});
+
 
 
 //========Listening=======
